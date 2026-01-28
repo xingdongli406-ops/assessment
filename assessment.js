7f17228c9839f24a11565baeed186b1fce0300c8
ネクストささらぎ <xingdongli406@gmail.com>
2026-01-28 13:42:17 +0900

    Bootstrapに対応

--- assessment.js
+++ assessment.js
@@ -25,14 +25,27 @@ assessmentButton.addEventListener(
 
   // TODO 診断結果表示エリアの作成
   resultDivision.innerText = '';
- const heading = document.createElement('h3');
- heading.innerText = '診断結果';
- resultDivision.appendChild(heading);
 
- const paragraph = document.createElement('p');
- const result = assessment(userName);
- paragraph.innerText = result;
- resultDivision.appendChild(paragraph);
+  //headerDivisionの作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class','card-header text-bg-primary');
+  headerDivision.innerText = '診断結果';
+  
+  // bodyDivisionの作成
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute('class','card-body');
+
+  const paragraph = document.createElement('p');
+  paragraph.setAttribute('class','card-text');
+  const result = assessment(userName);
+  paragraph.innerText = result;
+  bodyDivision.appendChild(paragraph);
+
+  // resultDivision に Bootstrap のスタイルを適用する
+  resultDivision.setAttribute('class','card');
+   // headerDivision と bodyDivision を resultDivision に差し込む
+   resultDivision.appendChild(headerDivision);
+   resultDivision.appendChild(bodyDivision);
 
 // TODO ツイートエリアの作成
 tweetDivision.innerText = '';
